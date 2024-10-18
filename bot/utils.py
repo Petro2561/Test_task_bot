c49158145bf5be79148845ebcc7fe8c8f4ecdc0c
Petr Andreev <105715065+Petro2561@users.noreply.github.com>
2024-10-18 17:47:01 +0300

    Update utils.py

--- bot/utils.py
+++ bot/utils.py
@@ -7,6 +7,5 @@ API_URL = "https://www.cbr.ru/scripts/XML_daily.asp"
 def get_usd_to_rub_rate():
     response = requests.get(API_URL)
     soup = BeautifulSoup(response.content, "xml")
-    print(soup)
     usd_value = soup.find("Valute", ID="R01235").Value.text
     return usd_value.replace(",", ".")
